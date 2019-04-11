3b112a5475c433dea900f6f730378ccce0524140
Alicja Operacz <alicja.operacz@gmail.com>
2019-04-11 11:53:01 +0200

    dodany constraint check dla skyscrappera

--- src/SGame.java
+++ src/SGame.java
@@ -34,6 +34,59 @@ public class SGame extends Game {
 
     @Override
     public boolean checkConstraints(int x, int y, int num) {
-        return false;
+        //z góry
+        int visible = 0;
+        int wanted = constraints[0][y];
+        int maxVal = board[0][y].value;
+        for(int i=0;i<=x;i++){
+            if(board[i][y].value>=maxVal){
+                maxVal = board[i][y].value;
+                visible++;
+            }
+            if(visible>wanted && wanted!=0){
+                return false;
+            }
+        }
+        //z dołu
+        visible = 0;
+        wanted = constraints[1][y];
+        maxVal = board[size-1][y].value;
+        for(int i=size-1;i>=x;i--){
+            if(board[i][y].value>=maxVal){
+                maxVal = board[i][y].value;
+                visible++;
+            }
+            if(visible>wanted && wanted!=0){
+                return false;
+            }
+        }
+        //z lewej
+        visible = 0;
+        wanted = constraints[2][y];
+        maxVal = board[x][0].value;
+        for(int i=0;i<=y;i++){
+            if(board[x][i].value>=maxVal){
+                maxVal = board[x][i].value;
+                visible++;
+            }
+            if(visible>wanted && wanted!=0){
+                return false;
+            }
+        }
+        //z prawej
+        visible = 0;
+        wanted = constraints[3][y];
+        maxVal = board[x][size-1].value;
+        for(int i=size-1;i>=y;i--){
+            if(board[x][i].value>=maxVal){
+                maxVal = board[x][i].value;
+                visible++;
+            }
+            if(visible>wanted && wanted!=0){
+                return false;
+            }
+        }
+
+        return true;
     }
 }
